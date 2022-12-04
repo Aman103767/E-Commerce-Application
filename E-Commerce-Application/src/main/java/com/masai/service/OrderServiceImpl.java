f90aa879a7575630467c4151c0a07e0562c769f0
Aman103767 <avinash103767@gmail.com>
2022-12-04 22:10:12 +0530

    update

--- E-Commerce-Application/src/main/java/com/masai/service/OrderServiceImpl.java
+++ E-Commerce-Application/src/main/java/com/masai/service/OrderServiceImpl.java
@@ -18,24 +18,27 @@ import com.masai.model.Customer;
 import com.masai.model.Orders;
 import com.masai.model.Product;
 import com.masai.repository.CartDao;
+import com.masai.repository.CustomerDao;
 import com.masai.repository.OrderDao;
 import com.masai.repository.ProductDao;
 import com.masai.repository.SessionDao;
 @Service
 public class OrderServiceImpl implements OrderService{
 	@Autowired
-	CartDao cdao;
+	private CartDao cdao;
 	
 	@Autowired
-	ProductDao pdao;
-	
+	private ProductDao pdao;
+	@Autowired
+	private CustomerDao custdao;
 	
 	@Autowired
-    OrderDao odao;
+    private OrderDao odao;
 	
 	@Autowired
-	SessionDao sessionDao;
+	private SessionDao sessionDao;
 
+	@SuppressWarnings("unused")
 	@Override
 	public List<Product> OrderProducts(Integer cartId,String key,Integer customerId) throws OrderException, CustomerException {
 		// TODO Auto-generated method stub
@@ -50,10 +53,16 @@ public class OrderServiceImpl implements OrderService{
         if(customerId == loggedInUser.getUserId()) {
        
 	
-		Optional<Cart> OptionalCart = cdao.findById(cartId);
+		Optional<Customer> OptionalCust = custdao.findById(customerId);
 		
-		if(OptionalCart.isPresent()) {
-			Cart cart = OptionalCart.get();
+		if(OptionalCust.isPresent()) {
+			Cart cart = OptionalCust.get().getCart();
+			if(cart.getCartproducts().size()==0) {
+				throw new OrderException("Please add the product in the cart first!");
+			}
+			if(cart == null) {
+				throw new OrderException("Please add the product in the cart first!");
+			}
 		Orders order = new Orders();
 		Customer cust = cart.getCustomer();
 		order.setAddress(cust.getAddress());
@@ -65,8 +74,11 @@ public class OrderServiceImpl implements OrderService{
 			
 		}
 		order.setProducts(products);
+		
 		odao.save(order);
-		return cart.getCartproducts();
+		cart.getCartproducts().clear();
+		cdao.save(cart);
+		return order.getProducts();
 		}
 		throw new OrderException("cart not found with id "+ cartId);
 		
